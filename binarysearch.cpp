6c65ef46261fee3fbe2fd79ba2cdfbf9ef282529
ilas-pakaya <ilaspakaya@ilas-2.local>
2025-05-07 20:21:50 +0700

    menampilkan elemen

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -9,7 +9,7 @@ void input()
 {
     while (true)
     {
-        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+        cout << "Masukkan banyaknya element pada array (maksimal 10): ";
         cin >> nPanjang;
         if (nPanjang <= 10)
         {
@@ -17,12 +17,12 @@ void input()
         }
         else
         {
-            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";
+            cout << "\n[!] Jumlah element tidak boleh lebih dari 10. Silahkan coba lagi.\n";
         }
     }
 
     cout << "\n===============================\n";
-    cout << "         Masukkan Elemen Array   \n";
+    cout << "         Masukkan Element Array   \n";
     cout << "=================================\n";
     for (int i = 0; i < nPanjang; i++)
     {
@@ -31,3 +31,36 @@ void input()
     }
 }
 
+void bubbleSortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang - 1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element[j + 1];
+                element[j + 1] = temp;
+            }
+        }
+        pass++;
+    } while (pass <= nPanjang - 1);
+}
+
+void display()
+{
+    cout << "\n=======================================\n";
+    cout << "   Element Array Setelah Diurutkan (Asc)  \n";
+    cout << "=========================================\n";
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+}
